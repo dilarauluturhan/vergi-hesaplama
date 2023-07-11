b11b0e2a31d61825e331be393725c32cce767d49
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-07-11 16:51:07 +0300

    feat: added header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        narnoor: ['Narnoor', 'serif']
+      },
+      colors: {
+        'redTax': '#FE0000',
+        'orangeTax': '#FBBE2F',
+        'brownTax': '#AE8C8A',
+        'blueTax': '#4AB1DA',
+        'grayTax': '#9C9C9C',
+        'greenTax': '#2FBD8D'
+      }
+    },
   },
   plugins: [],
 }
