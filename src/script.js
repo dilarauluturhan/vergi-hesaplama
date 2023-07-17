3a82c0b4e5e1192c383e6a74e0d17e42b621ffae
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-07-18 00:43:19 +0300

    feat: added tax include and tax except codes

--- src/script.js
+++ src/script.js
@@ -3,12 +3,12 @@ const rate_8 = document.getElementById('rate_8');
 const rate_18 = document.getElementById('rate_18');
 const rateRandom = document.getElementById('rateRandom');
 const rateInput = document.getElementById('rateInput');
-// const priceInput = document.querySelectorAll('#priceInput');
-// const taxInclude = document.querySelectorAll('#taxInclude');
-// const taxExcept = document.querySelectorAll('#taxExcept');
-// const subTotal = document.querySelectorAll('#subTotal');
-// const taxTotal = document.querySelectorAll('#taxTotal');
-// const total = document.querySelectorAll('#total');
+const priceInput = document.getElementById('priceInput');
+const taxInclude = document.getElementById('taxInclude');
+const taxExcept = document.getElementById('taxExcept');
+const subTotal = document.getElementById('subTotal');
+const taxTotal = document.getElementById('taxTotal');
+const total = document.getElementById('total');
 
 rate_1.addEventListener("click", (e) => {
     let clickedText = e.target.innerHTML;
@@ -32,7 +32,46 @@ rateRandom.addEventListener("click", function () {
 // yüzde girdiğimde klavye tuşu basılı olduğunda olayı dinlemesi için
 rateInput.addEventListener("keydown", (e) => {
     // sadece sayıları ve silme olayını kabul etmesi için
-    if (!(e.key === 'Delete' || e.key === '0' || e.key === '1' || e.key === '2' || e.key === '3' || e.key === '4' || e.key === '5' || e.key === '6' || e.key === '7' || e.key === '8' || e.key === '9')) {
+    if (!(e.key === 'Backspace' || e.key === 'Delete' || e.key === '0' || e.key === '1' || e.key === '2' || e.key === '3' || e.key === '4' || e.key === '5' || e.key === '6' || e.key === '7' || e.key === '8' || e.key === '9')) {
         e.preventDefault();
+    }
+})
+
+priceInput.addEventListener("keydown", (e) => {
+    // sadece sayıları ve silme olayını kabul etmesi için
+    if (!(e.key === 'Backspace' || e.key === 'Delete' || e.key === '0' || e.key === '1' || e.key === '2' || e.key === '3' || e.key === '4' || e.key === '5' || e.key === '6' || e.key === '7' || e.key === '8' || e.key === '9')) {
+        e.preventDefault();
+    }
+})
+
+taxInclude.addEventListener("click", () => {
+    let rateValue = rateInput.value; // yüzde değerini al
+
+    // yüzde değeri "%" işaretini kaldır
+    if (rateValue.startsWith("%")) {
+        rateValue = rateValue.substring(1);
+    }
+
+    const rate = parseFloat(rateValue); // rateInput'tan değeri alıp sayısal veriye dönüştürürüz
+    const price = parseFloat(priceInput.value);
+
+    // rate ve price sayısal değer ise işlemi yap
+    if (!isNaN(rate) && !isNaN(price)) {
+        const taxAmount = (rate / 100) * price;
+        const totalAmount = price + taxAmount;
+
+        subTotal.textContent = "₺" + price.toFixed(2); // virgülden sonra 2 basamakla sınırlandırır
+        taxTotal.textContent = "₺" + taxAmount.toFixed(2);
+        total.textContent = "₺" + totalAmount.toFixed(2);
+    }
+});
+
+taxExcept.addEventListener("click", () => {
+    const price = parseFloat(priceInput.value);
+
+    if (!isNaN(price)) {
+        subTotal.textContent = "₺" + price.toFixed(2);
+        taxTotal.textContent = "₺0.00";
+        total.textContent = "₺" + price.toFixed(2);
     }
 })
